09e0dde9632acb475fefe842e10d965bf26743f2
unknown <anhkhoadoan26022002@gmail.com>
2023-10-06 12:50:48 +0700

    test2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to Contactbook"});
+    res.json({message: "Welcome to Contactbook!!"});
 });
 
 
